373a33e4c8e2c26a2cdca6923584e1293f4d12c8
sdoshi579 <sanket.doshi@pharmeasy.in>
2021-05-16 00:56:01 +0530

    use defined multiplexer, go mod init, remodifying the codebase

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sdoshi579/go-practice
+
+go 1.16
--- main.go
+++ main.go
@@ -1,44 +1,9 @@
 package main
 
-import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Customer struct {
-	Name 	string 	`json:"full_name"`
-	City 	string 	`json:"city"`
-	Zipcode string 	`json:"zip_code"`
-}
+import "github.com/sdoshi579/go-practice/app"
 
 func main() {
 
-	//create routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	// starting a server
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	app.Start()
 }
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World !!!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{"Sharukh", "Mumbai", "400086"},
-		{"Amitabh", "Mumbai", "400086"},
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
-	}
-}
