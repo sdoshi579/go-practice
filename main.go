65daaf769519652b4098e3971b6bdac2aaf507a1
sdoshi579 <sanket.doshi@pharmeasy.in>
2021-05-15 16:25:07 +0530

    send json response

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name 	string 	`json:"full_name"`
+	City 	string 	`json:"city"`
+	Zipcode string 	`json:"zip_code"`
+}
+
 func main() {
 
 	//create routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting a server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -17,4 +25,13 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World !!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Sharukh", "Mumbai", "400086"},
+		{"Amitabh", "Mumbai", "400086"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
