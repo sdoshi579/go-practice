6d6d67445e8d4e0c502560a425eb81b298e8ef83
sdoshi579 <sanket.doshi@pharmeasy.in>
2021-05-24 21:52:34 +0530

    use DTO

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/sdoshi579/go-practice/errs"
+import (
+	"github.com/sdoshi579/go-practice/dto"
+	"github.com/sdoshi579/go-practice/errs"
+)
 
 type Customer struct {
 	Id			string	`db:"customer_id"`
@@ -14,4 +17,22 @@ type Customer struct {
 type CustomerRepository interface {
 	FindAll(string) ([]Customer, *errs.AppError)
 	FindById(string) (*Customer, *errs.AppError)
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id: 			c.Id,
+		Name: 			c.Name,
+		Status: 		c.getStatusAsText(),
+		DateOfBirth: 	c.DateOfBirth,
+		Zipcode: 		c.Zipcode,
+		City: 			c.City,
+	}
+}
+
+func (c Customer) getStatusAsText() string {
+	if c.Status == "0" {
+		return "inactive"
+	}
+	return "active"
 }
--- service/customerService.go
+++ service/customerService.go
@@ -1,11 +1,14 @@
 package service
 
-import "github.com/sdoshi579/go-practice/domain"
-import "github.com/sdoshi579/go-practice/errs"
+import (
+	"github.com/sdoshi579/go-practice/domain"
+	"github.com/sdoshi579/go-practice/dto"
+	"github.com/sdoshi579/go-practice/errs"
+)
 
 type CustomerService interface {
 	GetAllCustomer(string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
@@ -16,8 +19,15 @@ func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer
 	return s.repo.FindAll(status)
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.FindById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.FindById(id)
+
+	if err != nil {
+		return nil, err
+	}
+	response := c.ToDto()
+
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
